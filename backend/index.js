b4fc0c7c9553671242ebae4e6a65f6e456086a34
Alok Kumar <rajalok957641@gmail.com>
2026-03-12 20:29:55 +0530

    fix fetch typo

--- backend/index.js
+++ backend/index.js
@@ -11,45 +11,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
-
 app.post("/send-email", async (req, res) => {
 
-  const { name, email, message } = req.body;
+  console.log("BODY DATA:", req.body);
 
-  try {
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      replyTo: email,
-      to: process.env.EMAIL_USER,
-      subject: "New Contact Form",
-      html: `
-        <h3>New Contact Form</h3>
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Message:</b> ${message}</p>
-      `,
-    });
-
-    res.json({ success: true });
-
-  } catch (error) {
-    console.log("EMAIL ERROR 👉", error);
-    res.status(500).json({ success: false });
-  }
+  res.json({
+    success: true,
+    msg: "API working"
+  });
 
 });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
